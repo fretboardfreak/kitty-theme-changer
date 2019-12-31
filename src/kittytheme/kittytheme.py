0535672b6810d0737fe7e5bc13eef138cd2e01fa
Curtis Sand <curtissand@gmail.com>
2019-12-30 17:11:28 -0800

    replace hardcoded config for a config module
    Also provide a "--help-config" flag to provide the user with help
    configuring the script.

--- src/kittytheme/kittytheme.py
+++ src/kittytheme/kittytheme.py
@@ -18,6 +18,8 @@
 import argparse
 import sys
 
+from importlib.util import spec_from_file_location
+from importlib.util import module_from_spec
 from pathlib import Path
 from subprocess import call
 
@@ -27,29 +29,48 @@ VERBOSE = False
 DEBUG = False
 DEBUG_OUTPUT_PREFIX = 'debug: '
 
-
-class Config(object):
-    """A container object to hold the Config that this script works with."""
-    theme_dir = Path('~/storage/lib/kitty-themes/themes').expanduser()
-    conf_dir = Path('~/.config/kitty').expanduser()
-    theme_link = conf_dir.joinpath('theme.conf')
-    light_theme_link = conf_dir.joinpath('light-theme.conf')
-    dark_theme_link = conf_dir.joinpath('dark-theme.conf')
-    socket = 'unix:/tmp/kittysocket'
+DEFAULT_CONFIG = '~/.kittythemechanger.py'
 
 
 def main():
     """Main script logic."""
     args = parse_cmd_line()
 
+    # Load Config Module
+    spec = spec_from_file_location(args.config.stem, args.config.as_posix())
+    config = module_from_spec(spec)
+    spec.loader.exec_module(config)
+    check_config(config)
+
     if args.action:
         dprint('calling action: {}'.format(args.action))
-        getattr(Actions, args.action)(args)
-    else: # take default action
+        getattr(Actions, args.action)(args, config)
+    else:  # take default action
         dprint('no action provided: calling default action')
-        Actions.show(args)
+        Actions.show(args, config)
 
     return 0
+
+
+def existing_file(input_text):
+    """Ensure the input text is an existing file path."""
+    print('input config file: {}'.format(input_text))
+    filepath = Path(input_text).expanduser()
+    if not filepath.exists():
+        raise argparse.ArgumentTypeError(
+            'The configuration file must exist. Use --help-config to see '
+            'how to configure the Kitty Theme Changer.')
+    return filepath
+
+
+def check_theme_provided(args):
+    """Ensure actions that need a theme input receive one."""
+    action_needs_theme = ['test', 'set_dark', 'set_light']
+    if args.action and args.action in action_needs_theme:
+        dprint('action {} requires a theme from the user.'.format(args.action))
+        if not args.theme:
+            msg = 'The "{}" action requires a theme name.'.format(args.action)
+            raise argparse.ArgumentError(msg)
 
 
 def parse_cmd_line():
@@ -60,6 +81,8 @@ def parse_cmd_line():
         version='%(prog)s {}'.format(VERSION))
     DebugAction.add_parser_argument(parser)
     VerboseAction.add_parser_argument(parser)
+    parser.add_argument(
+        '-c', '--config', type=existing_file, default=DEFAULT_CONFIG)
     parser.add_argument(
         '-l', '--list', action="store_const", const='list', dest='action',
         default='', help="List available themes.")
@@ -85,32 +108,56 @@ def parse_cmd_line():
         action='store', dest='theme', metavar='THEME', nargs='?', default='',
         help=("The theme name to use. Use --list to get a list "
               "of available themes."))
+    parser.add_argument(
+        '--help-config', action='store_true', dest='config_help',
+        default=False,
+        help="Print info on how to configure Kitty Theme Changer.")
 
     args = parser.parse_args()
-
-    # ensure actions that need a theme input receive one
-    action_needs_theme = ['test', 'set_dark', 'set_light']
-    if args.action and args.action in action_needs_theme:
-        dprint('action {} requires a theme from the user.'.format(args.action))
-        if not args.theme:
-            msg = 'The "{}" action requires a theme name.'.format(args.action)
-            parser.error(msg)
+    if args.config_help:
+        print_config_help()
+        sys.exit(0)
+    check_theme_provided(args)
     return args
 
 
-def list_themes(args):
+def check_config(config):
+    """Check that the config module has the required attributes."""
+    required_config_attributes = [
+        ('theme_dir', Path),
+        ('conf_dir', Path),
+        ('theme_link', Path),
+        ('light_theme_link', Path),
+        ('dark_theme_link', Path),
+        ('socket', str)
+    ]
+    dprint('checking config: {}'.format(dir(config)))
+    dprint(config.__file__)
+    for attribute, type in required_config_attributes:
+        check_status = True
+        try:
+            check_status = isinstance(getattr(config, attribute), type)
+        except AttributeError:
+            check_status = False
+        if not check_status:
+            raise Exception(
+                'The config module is missing a variable named '
+                '{} of type {}'.format(attribute, type))
+
+
+def list_themes(args, config):
     """List the available themes."""
     print('Available Kitty Themes:')
-    dprint('Looking for themes in: {}'.format(Config.theme_dir))
-    for theme in sorted(Config.theme_dir.glob('*conf'),
+    dprint('Looking for themes in: {}'.format(config.theme_dir))
+    for theme in sorted(config.theme_dir.glob('*conf'),
                         key=lambda s: s.stem.lower()):
         print('  {}'.format(theme.stem))
 
 
-def show_config(args):
+def show_config(args, config):
     """Show the current theme configuration."""
-    dprint('looking at symlink target of {}'.format(Config.theme_link))
-    theme = Config.theme_link.resolve().stem
+    dprint('looking at symlink target of {}'.format(config.theme_link))
+    theme = config.theme_link.resolve().stem
     print('current configured theme is: {}'.format(theme))
 
 
@@ -119,7 +166,7 @@ def get_theme_file(theme_name):
 
     Return None if the file does not exist.
     """
-    theme_file = Config.theme_dir.joinpath('{}.conf'.format(theme_name))
+    theme_file = config.theme_dir.joinpath('{}.conf'.format(theme_name))
     dprint('theme_file: {}'.format(theme_file))
     if not theme_file.exists():
         print('Provided theme does not exist. Use the '
@@ -128,69 +175,111 @@ def get_theme_file(theme_name):
     return theme_file
 
 
-def test_theme(args):
+def test_theme(args, config):
     """Test the given theme in the current kitty session."""
     theme_file = get_theme_file(args.theme)
     vprint('Changing theme of current kitty window to: {}'.format(
         theme_file.name))
-    cmd = ['kitty', '@', '--to={}'.format(Config.socket), 'set-colors',
+    cmd = ['kitty', '@', '--to={}'.format(config.socket), 'set-colors',
            theme_file]
     dprint('executing: {}'.format(' '.join(cmd)))
     call(cmd)
 
 
-def toggle_themes(args):
+def toggle_themes(args, config):
     """Toggle the themes between light and dark."""
     vprint('Toggling configured theme between light and dark.')
-    if Config.light_theme_link.resolve() == Config.theme_link.resolve():
+    if config.light_theme_link.resolve() == config.theme_link.resolve():
         # light theme: reconfig to dark theme
         dprint('light theme configured. enabling dark theme.')
-        Config.theme_link.unlink()
-        Config.theme_link.symlink_to(Config.dark_theme_link)
-    elif Config.dark_theme_link.resolve() == Config.theme_link.resolve():
+        config.theme_link.unlink()
+        config.theme_link.symlink_to(config.dark_theme_link)
+    elif config.dark_theme_link.resolve() == config.theme_link.resolve():
         # dark theme: reconfig to light theme
         dprint('dark theme configured. enabling light theme.')
-        Config.theme_link.unlink()
-        Config.theme_link.symlink_to(Config.light_theme_link)
+        config.theme_link.unlink()
+        config.theme_link.symlink_to(config.light_theme_link)
     else:
         print('Configured theme.conf does not match configured '
               'light-theme.conf or dark-theme.conf. Setting theme to dark.')
-        args.theme = Config.dark_theme_link.resolve().stem
-        set_dark_theme(args)
+        args.theme = config.dark_theme_link.resolve().stem
+        set_dark_theme(args, config)
 
 
-def set_dark_theme(args):
+def set_dark_theme(args, config):
     """Set the default theme to the configured dark theme."""
     theme_file = get_theme_file(args.theme)
     dprint('existing dark theme is: {}'.format(
-        Config.dark_theme_link.resolve()))
+        config.dark_theme_link.resolve()))
     vprint('Changing configured dark theme to {}'.format(theme_file.name))
-    Config.dark_theme_link.unlink()
-    Config.dark_theme_link.symlink_to(theme_file)
+    config.dark_theme_link.unlink()
+    config.dark_theme_link.symlink_to(theme_file)
 
 
-def set_light_theme(args):
+def set_light_theme(args, config):
     """Set the default theme to the configured light theme."""
     theme_file = get_theme_file(args.theme)
     dprint('existing light theme is: {}'.format(
-        Config.light_theme_link.resolve()))
+        config.light_theme_link.resolve()))
     vprint('Changing configured light theme to {}'.format(theme_file.name))
-    Config.dark_theme_link.unlink()
-    Config.dark_theme_link.symlink_to(theme_file)
+    config.dark_theme_link.unlink()
+    config.dark_theme_link.symlink_to(theme_file)
 
 
-def make_theme_live(args):
+def make_theme_live(args, config):
     """Update all existing kitty sessions to use the configured theme."""
     vprint('Changing theme of all running kitty windows to: {}'.format(
-        Config.theme_link.resolve().name))
-    cmd = ['kitty', '@', '--to={}'.format(Config.socket), 'set-colors',
-           '--all', Config.theme_link]
+        config.theme_link.resolve().name))
+    cmd = ['kitty', '@', '--to={}'.format(config.socket), 'set-colors',
+           '--all', config.theme_link]
     dprint('executing: {}'.format(' '.join(cmd)))
     call(cmd)
 
 
-class Actions(object):
+def print_config_help():
+    """Print a help message about configuring Kitty Theme Changer."""
+    msg = "Configuring Kitty Theme Changer\n\n"
+    msg += "1. First step in configuring the Kitty Theme Changer is to \n"
+    msg += "   download a set of themes for kitty \n"
+    msg += "   (recommendation: https://github.com/dexpota/kitty-themes).\n\n"
+    msg += "2. Second step is to configure kitty to include a theme config \n"
+    msg += "   file. To do this add the line 'include ./theme.conf' in your \n"
+    msg += "   kitty.conf file.\n\n"
+    msg += "3. Third step is to add the symlinks that the Kitty Theme \n"
+    msg += "   Changer uses to toggle between a light and a dark theme.\n"
+    msg += "   Create these inside '~/.config/kitty/' with:\n"
+    msg += "       ln -s /path/to/kitty/themes/sometheme.conf light-theme.conf\n"
+    msg += "       ln -s /path/to/kitty/themes/othertheme.conf dark-theme.conf\n"
+    msg += "       ln -s theme.conf dark-theme.conf\n"
+    msg += "4. Fourth and final step is to create the Kitty Theme Changer \n"
+    msg += "   config file which will point to the correct paths for the \n"
+    msg += "   themes you've collected.\n"
+    msg += "   The Kitty theme changer uses a simple python module as \n"
+    msg += "   a configuration file. By default this is '{}'.\n".format(
+        DEFAULT_CONFIG)
+    msg += "   The list of required variables and their types are:\n"
+    msg += "     theme_dir (pathlib.Path): Directory of Kitty theme.conf files.\n"
+    msg += "     conf_dir (pathlib.Path): Directory Kitty looks in for theme.conf\n"
+    msg += "     theme_link (pathlib.Path): Symlink file Kitty loads from kitty.conf\n"
+    msg += "     light_theme_link (pathlib.Path): Symlink to a 'light' theme config file.\n"
+    msg += "     dark_theme_link (pathlib.Path): Symlink to a 'dark' theme config file.\n"
+    msg += "     socket (str): a Kitty compatible socket string for the \n"
+    msg += "                   '--listen-on' flag. See 'man kitty'.\n\n"
+    msg += "An example .kittythemechanger.py file is shown below::\n\n"
+    msg += '"""A container object to hold the Config that this script works with."""\n'
+    msg += "from pathlib import Path\n"
+    msg += "theme_dir = Path('~/kitty-themes/themes').expanduser()\n"
+    msg += "conf_dir = Path('~/.config/kitty').expanduser()\n"
+    msg += "theme_link = conf_dir.joinpath('theme.conf')\n"
+    msg += "light_theme_link = conf_dir.joinpath('light-theme.conf')\n"
+    msg += "dark_theme_link = conf_dir.joinpath('dark-theme.conf')\n"
+    msg += "socket = 'unix:/tmp/kittysocket'\n"
+    print(msg)
+
+
+class Actions:
     """A container object to hold the actions this script can perform."""
+
     list = list_themes
     show = show_config
     test = test_theme
@@ -221,13 +310,16 @@ class DebugAction(argparse.Action):
 
     @classmethod
     def add_parser_argument(cls, parser):
+        """Add arguments for this action to an argparse parser object."""
         parser.add_argument(cls.sflag, cls.flag, help=cls.help, action=cls)
 
     def __init__(self, option_strings, dest, **kwargs):
+        """Initialize this Action object."""
         super(DebugAction, self).__init__(option_strings, dest, nargs=0,
                                           default=False, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
+        """Set DEBUG to True for this execution."""
         global DEBUG
         DEBUG = True
         setattr(namespace, self.dest, True)
@@ -241,6 +333,7 @@ class VerboseAction(DebugAction):
     help = 'Enable verbose output.'
 
     def __call__(self, parser, namespace, values, option_string=None):
+        """Set VERBOSE to True for this execution."""
         global VERBOSE
         VERBOSE = True
         setattr(namespace, self.dest, True)
